2709881dd15d162e2e8a29172878cfee20d3a271
Dan Trusca <truscadani@yahoo.com>
2020-10-07 21:57:48 +0300

    setting authentication with Passport

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
+const passport = require("passport");
+
+require("./models/User");
+require("./services/passport");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+const db = keys.mongoURI;
+//connect to MongoDB
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+//middlewares
+app.use(express.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
