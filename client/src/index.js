9f36c293a3f91ce07496307d58f3f56aeacce433
Dan Trusca <truscadani@yahoo.com>
2020-10-09 21:04:10 +0300

    working on client side, on header

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,33 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware, compose} from "redux";
+import thunk from "redux-thunk";
+
+import rootReducer from "./reducers/index"
+
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+
+const middleWare = [thunk];
+
+const store = createStore(
+  rootReducer,
+  {},
+  compose(
+    applyMiddleware(...middleWare),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
