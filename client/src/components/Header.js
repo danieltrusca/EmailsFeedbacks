6861e41a121ba16918123f3a93029cc322881bb5
Dan Trusca <truscadani@yahoo.com>
2020-10-12 17:10:08 +0300

    handling payment with Stripe

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 
+import Payments from "./Payments";
+
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
         return;
       case false:
-        return <li><a href="/auth/google">Login With Google</a></li>;
+        return <li key={1}><a href="/auth/google">Login With Google</a></li>;
       default:
-        return <li><a href="/api/logout">Logout</a></li>;
+        return  [
+          <li key="3"><Payments /></li>,
+          <li key="4" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="5"><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
