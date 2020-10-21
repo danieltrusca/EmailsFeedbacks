409642622632e0e55cd23779c9d0821977aedfcb
Dan Trusca <truscadani@yahoo.com>
2020-10-21 15:41:56 +0300

    handling webhook data

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _=require("lodash");
+const { Path } = require('path-parser');
+const {URL}=require("url");
 const mongoose=require("mongoose");
 const requireLogin=require("../middleware/requireLogin");
 const requireCredits=require("../middleware/requireCredits");
@@ -8,10 +11,64 @@ const Survey=mongoose.model("surveys")
 
 module.exports = app => {
 
-    app.get('/api/surveys/thanks', (req, res) => {
-        res.send('Thanks for voting!');
-      });
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('Thanks for voting!');
+  });
 
+      // app.post("/api/surveys/webhooks",(req, res)=>{
+      //   const events=_.map(req.body, ({email, url})=>{
+      //     const pathname=new URL(url).pathname;
+      //     const p = new Path('/api/surveys/:surveyId/:choice');
+      //     const match=p.test(pathname);
+      //     if (match) {
+      //       return { email, surveyId: match.surveyId, choice: match.choice };
+      //     }
+      //   });
+
+      //   //remove all undefined records with compact from lodash
+      //   const compactEvents = _.compact(events);
+
+      //   //we use unigBy from lodash to get just the unique records from array
+      //   const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+      //   console.log(uniqueEvents);
+
+      //   res.send({});
+      // });
+
+      app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+    
+        _.chain(req.body)
+          .map(({ email, url }) => {
+            const match = p.test(new URL(url).pathname);
+            if (match) {
+              return { email, surveyId: match.surveyId, choice: match.choice };
+            }
+          })
+          .compact()
+          .uniqBy('email', 'surveyId')
+          .each(({ surveyId, email, choice }) => {
+            Survey.updateOne(
+              {
+                _id: surveyId,
+                recipients: {
+                  $elemMatch: { email: email, responded: false }
+                }
+              },
+              {
+                $inc: { [choice]: 1 },
+                $set: { 'recipients.$.responded': true },
+                lastResponded: new Date()
+              }
+            ).exec();
+          })
+          .value();
+    
+        
+    
+        res.send({});
+      });
 
     app.post('/api/surveys', requireLogin, requireCredits ,async (req, res) => {
         const {title, subject, body, recipients}=req.body;
